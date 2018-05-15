b51343c6894a6415e5ce59ee34e735a1028516ea
Gaspard Francony <GFR3788@viseo.net>
2018-05-15 18:08:19 +0200

    Use of material for join form, add redirect

--- app/join-phonograph/join-phonograph.component.js
+++ app/join-phonograph/join-phonograph.component.js
@@ -4,13 +4,24 @@ angular
     .module('joinPhonograph', ['ngRoute'])
     .component('joinPhonograph', {
         templateUrl: 'join-phonograph/join-phonograph.template.html',
-        controller: function JoinPhonographController() {
+        controller: function JoinPhonographController($location) {
             var self = this;
+            self.serverError = '';
+            self.phonographId = '';
             self.phonographs = [
                 {
                     id: '123',
                     name: 'WOLOLO'
                 }
             ];
+            self.submit = function () {
+                // TBD : Mock services here, change validation
+                if (self.phonographs.find(item => item.id === self.phonographId) === undefined) {
+                    self.serverError = 'notfound';
+                }
+                else {
+                    $location.path('/'+self.phonographId)
+                }
+            };
         }
     });
