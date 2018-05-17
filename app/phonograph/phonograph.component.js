fa1f88b11dd9b7f3d19d9bb761bc78070c444cf6
Gaspard Francony <GFR3788@viseo.net>
2018-05-17 11:21:04 +0200

    Redirect if no phonograh in phonograph component

--- app/phonograph/phonograph.component.js
+++ app/phonograph/phonograph.component.js
@@ -4,11 +4,15 @@ angular.
     module('phonograph', ['ngRoute', 'phonographService']).
     component('phonograph', {
         templateUrl: 'phonograph/phonograph.template.html',
-        controller: ['$routeParams', 'PhonographService',
-            function PhonographController($routeParams, PhonographService) {
+        controller: ['$location', '$routeParams', 'PhonographService',
+            function PhonographController($location, $routeParams, PhonographService) {
                 var self = this;
                 self.order = 'name';
                 self.phonograph = PhonographService.get($routeParams.phonographId);
+                // In case there is no corresponding Phonograph, redirect to join
+                if (self.phonograph === undefined) {
+                    $location.path('/join')
+                }
             }
         ]
     });
